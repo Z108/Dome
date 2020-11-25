3116d96bc23e0043fa053911fdff3c835bd53883
zgsgx <1084559628@qq.com>
2020-11-25 09:22:46 +0800

    no message

---
+++ h5/js/style.js
@@ -0,0 +1,28 @@
+let hour = document.querySelector('.hour')
+let mintue = document.querySelector('.mintue')
+let second = document.querySelector('.second')
+
+let timer =setInterval(time, 1000)
+function time() {
+	let now = new Date();
+	let targetTime = new Date('2020/11/25,00:00:00')
+	let difference = (targetTime - now) / 1000
+	console.log(targetTime - now)
+	if(difference<0){
+		console.log(timer)
+		clearInterval(timer)
+		return
+	}
+	let D = addZero(Math.floor(difference / 3600 / 24 % 30))
+	let H = addZero(Math.floor(difference / 3600 % 24))
+	let M = addZero(Math.floor(difference / 60 % 60))
+	let S = addZero(Math.floor(difference % 60))
+	hour.innerText = H
+	mintue.innerText = M
+	second.innerText = S
+}
+
+
+function addZero(time) {
+	return time >= 10 ? time : '0' + time
+}
