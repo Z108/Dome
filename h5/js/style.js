6511e177eadd651c22249207c82887ffc8d4328f
zgsgx <1084559628@qq.com>
2020-12-10 14:04:25 +0800

    no message

--- h5/js/style.js
+++ h5/js/style.js
@@ -1,5 +1,7 @@
 let banner = document.querySelector('#banner')
-
+let dote = document.querySelector('.dote')
+let next = document.querySelectorAll('#warp>span')
+var count = 0
 getData()
 async function getData() {
 	let {
@@ -15,25 +17,40 @@ async function getData() {
 
 function writeDom(data) {
 	let str = ''
+	let liList=''
 	data.forEach((v, i) => {
 		str += `<img src="${v.img}" alt="${v.name}">`
+		liList+='<li></li>'
 	})
 	banner.innerHTML = str
+	dote.innerHTML = liList
 	swiper()
 
 }
 
 function swiper() {
 	let img = document.querySelectorAll('img')
-	var count = 0
+	let liList = document.querySelectorAll('.dote>li')
 	setInterval(() => {
 		count = count >= 0 ? count < img.length ? +count+1 :0: 0
 		for(let i=0;i<img.length;i++){
 			img[i].style.display = "none";
 		}
-		
-		console.log(count)
 		img[count].style.display = "block";
 	}, 3000)
-
+	for(let i=0;i<next.length;i++){
+	next[i].addEventListener('click',()=>{
+		if(i==0){
+			count = count > 0 ? count-- :img.length
+		}else{
+			count = count >= 0 ? count < img.length ? +count+1 :0: 0
+		}
+		for(let i=0;i<img.length;i++){
+			img[i].style.display = "none";
+		}
+		img[count].style.display = "block";
+		
+	})
 }
+}
+
